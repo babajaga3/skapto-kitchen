97226b7d6131044da71d27937b51a8b34032b07f
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-15 18:25:10 +0300

    :zap: Create settings item component

---
+++ src/components/settings-item.tsx
@@ -0,0 +1,24 @@
+import { Microwave } from "lucide-react"
+import { ReactNode } from "react"
+
+export function SettingsItem({
+  title,
+  description,
+  item
+}: {
+  title: string
+  description: string
+  item: ReactNode
+}) {
+  return (
+    <div className="flex flex-row items-center gap-8">
+      <div className="flex flex-col items-start">
+        <h1 className="text-muted-foreground font-semibold">{title}</h1>
+        <p className="text-base ">{description}</p>
+      </div>
+      <div>       
+        {item}
+      </div>
+    </div>
+  )
+}
