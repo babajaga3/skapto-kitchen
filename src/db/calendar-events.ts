05bfaca945f871738bd782c093cb48ca70e6e41f
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-17 10:41:50 +0300

    :zap: Create calendar events class

---
+++ src/db/calendar-events.ts
@@ -0,0 +1,40 @@
+import { pb } from '@/lib/pocketbase'
+import { CalendarEvent } from '@/types/calendar-events'
+import { RecordModel } from 'pocketbase'
+
+
+// Type for creating calendar event record
+type CreateCalendarEvent = Omit<CalendarEvent, 'id' | 'created' | 'updated'>
+
+export class CalendarEvents {
+  // Calendar collection var (less boilerplate)
+  private static readonly calendarCollection = pb.collection('calendar_events')
+  
+  // Get all events
+  public static async getAll(filter?: string): Promise<RecordModel[]> {
+    const data = await CalendarEvents.calendarCollection.getFullList({
+      filter
+    })
+    
+    return data
+  }
+  
+  // Get one event
+  public static async getOne(recordId: string) {
+    const data = await CalendarEvents.calendarCollection.getOne(recordId)
+    
+    return data
+  }
+  
+  // Create an event
+  public static async create(calendarEvent: CreateCalendarEvent) {
+    const data = await CalendarEvents.calendarCollection.create<CalendarEvent>(calendarEvent)
+    
+    return data
+  }
+  
+  // Delete an event
+  public static async delete(recordId: string) {
+    await CalendarEvents.calendarCollection.delete(recordId)
+  }
+}
