13a1b9966b30d8fa555e50bcb4536914e7367368
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-26 19:31:52 +0300

    :hammer: Minor type updates

--- src/providers/kitchen-provider.tsx
+++ src/providers/kitchen-provider.tsx
@@ -9,26 +9,30 @@ import { SkaptoKitchens } from '@/types/calendar-events'
 export interface KitchenContextValue {
   kitchen: SkaptoKitchens | undefined
   setKitchen: (kitchen: SkaptoKitchens) => void
+  getKitchen: () => SkaptoKitchens | undefined
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const KitchenContext = createContext<KitchenContextValue | null>(null)
 
 export const KitchenProvider = ({ children }: { children: React.ReactNode }) => {
-  const [ kitchen, setKitchenState ] = useState<SkaptoKitchens | undefined>(undefined)
+  const [ kitchen, setKitchenState ] = useState<KitchenContextValue['kitchen']>(undefined)
 
+  // Fetch Kitchen Cookie and set the state
   useEffect(() => {
-    const stored = Cookies.get('main-kitchen') as SkaptoKitchens | undefined
+    const stored = Cookies.get('main-kitchen') as KitchenContextValue['kitchen']
     if (stored) setKitchenState(stored)
   }, [])
 
   const setKitchen = useCallback((newKitchen: SkaptoKitchens) => {
-    Cookies.set('main-kitchen', newKitchen, { expires: 365 })
+    Cookies.set('main-kitchen', newKitchen, { expires: 130 }) // Approx. 1 uni semester todo check later for accurate
     setKitchenState(newKitchen)
   }, [ setKitchenState ])
 
+  const getKitchen = useCallback(() => Cookies.get('main-kitchen') as KitchenContextValue['kitchen'], [])
+
   return (
-    <KitchenContext.Provider value={{ kitchen, setKitchen }}>
+    <KitchenContext.Provider value={{ kitchen, setKitchen, getKitchen }}>
       {children}
     </KitchenContext.Provider>
   )
