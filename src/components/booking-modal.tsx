80803e7db98e1a1591bb790b6145c3aa6839a96c
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-29 19:07:00 +0200

    ⚡ Add drawer for mobile views

--- src/components/booking-modal.tsx
+++ src/components/booking-modal.tsx
@@ -4,14 +4,36 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import { useBookingModal } from '@/hooks/use-booking-modal'
 import { useIsMobile } from '@/hooks/use-mobile'
 import { BookingForm } from '@/components/booking-form'
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from './ui/drawer'
 
 
 export function BookingModal() {
   const { open, openMobile, toggleModal } = useBookingModal()
   const isMobile = useIsMobile()
 
+  if (isMobile) {
+    return (
+      <Drawer open={openMobile} onOpenChange={toggleModal}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Create new booking</DrawerTitle>
+            <DrawerDescription>
+              Book the kitchen by setting the desired hour and date when you want to do that.
+              You may not book the kitchen for more than 2 hours per day.
+            </DrawerDescription>
+          </DrawerHeader>
+
+          <div className={'p-4 overflow-auto'}>
+            <BookingForm />
+          </div>
+        </DrawerContent>
+      </Drawer>
+
+    )
+  }
+
   return (
-    <Dialog open={isMobile ? openMobile : open} onOpenChange={toggleModal}>
+    <Dialog open={open} onOpenChange={toggleModal}>
       <DialogContent>
 
         {/* Header */}
