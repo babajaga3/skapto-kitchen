84561dc90e2da4c23d41160925086e9267022997
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-30 15:04:32 +0200

    :zap: Implement list calendar

--- src/components/list-calendar.tsx
+++ src/components/list-calendar.tsx
@@ -5,14 +5,31 @@ import listPlugin from '@fullcalendar/list'
 import { useQuery } from '@tanstack/react-query'
 import { CalendarEvents } from '@/db/calendar-events'
 import { useKitchen } from '@/hooks/use-kitchen'
+import { useIsMobile } from '@/hooks/use-mobile'
+import { useCallback, useMemo, useState } from 'react'
+import { PBFilters } from '@/lib/pocketbase'
 
 
 export function ListCalendar() {
+  const isMobile = useIsMobile()
   const { kitchen } = useKitchen()
+  const [ studentId, setStudentId ] = useState<number>()
 
+  /*
+  * TODO:
+  *  - Maintain filters after changing kitchen
+  *  - Maintain filters after clicking off page (don't maintain for reloading the page)
+  * */
+
+  // Filters for the query
+  const queryFilters = useMemo(() => {
+    return new PBFilters([{ key: 'kitchen', value: kitchen ?? '' }])
+  }, [ kitchen ])
+
+  // Query
   const eventsQuery = useQuery({
-    queryKey: [ 'events', 'all', kitchen ],
-    queryFn: () => CalendarEvents.getAll(`kitchen = '${kitchen}'`),
+    queryKey: [ 'events', 'all', kitchen, studentId ],
+    queryFn: () => CalendarEvents.getAll(queryFilters.toString()),
     select: data => data.map(event => {
       return {
         title: event.studentName,
@@ -22,6 +39,17 @@ export function ListCalendar() {
     })
   })
 
+  // Function to filter bookings (all and user-only)
+  const filterBookings = useCallback((studentId: number | null) => {
+    if (studentId !== null) {
+      queryFilters.update('studentId', studentId)
+      setStudentId(studentId)
+    } else {
+      queryFilters.remove('studentId')
+      setStudentId(undefined)
+    }
+  }, [ queryFilters ])
+
   return (
     <div className='h-full w-full overflow-auto'>
       <FullCalendar
@@ -29,6 +57,21 @@ export function ListCalendar() {
         plugins={[ listPlugin ]}
         initialView='listWeek'
         height={'100%'}
+        headerToolbar={{
+          left: isMobile ? 'mine all' : 'title',
+          center: !isMobile ? 'mine all' : '',
+          right: `${isMobile ? '' : 'today '}prev,next`
+        }}
+        customButtons={{ // todo figure out better name
+          all: {
+            text: 'All bookings',
+            click: () => filterBookings(null)
+          },
+          mine: {
+            text: 'My bookings',
+            click: () => filterBookings(200274715) // todo hardcoded for now, should be dynamic
+          }
+        }}
       />
     </div>
   )
