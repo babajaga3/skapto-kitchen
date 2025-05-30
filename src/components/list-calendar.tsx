d6ba713a8131bba0f636c3bc632c78def05ba671
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-05-30 10:56:12 +0200

    :zap: Display all events on list calendar

--- src/components/list-calendar.tsx
+++ src/components/list-calendar.tsx
@@ -2,13 +2,30 @@
 
 import FullCalendar from '@fullcalendar/react'
 import listPlugin from '@fullcalendar/list'
+import { useQuery } from '@tanstack/react-query'
+import { CalendarEvents } from '@/db/calendar-events'
+import { useKitchen } from '@/hooks/use-kitchen'
 
 
 export function ListCalendar() {
+  const { kitchen } = useKitchen()
+
+  const eventsQuery = useQuery({
+    queryKey: [ 'events', 'all', kitchen ],
+    queryFn: () => CalendarEvents.getAll(`kitchen = '${kitchen}'`),
+    select: data => data.map(event => {
+      return {
+        title: event.studentName,
+        start: event.start,
+        end: event.end
+      }
+    })
+  })
 
   return (
     <div className='h-full w-full overflow-auto'>
       <FullCalendar
+        events={eventsQuery?.data}
         plugins={[ listPlugin ]}
         initialView='listWeek'
         height={'100%'}
