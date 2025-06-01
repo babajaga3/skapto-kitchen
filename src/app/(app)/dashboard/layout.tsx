fad0a233e46ed81f1aebd1b4bda99e2d51b5fc37
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-06-01 14:23:44 +0200

    :zap: Add refresh bookings button

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -5,14 +5,32 @@ import { Button } from '@/components/ui/button'
 import { useSidebar } from '@/components/ui/sidebar'
 import { UserMenu } from '@/components/user-menu'
 import { useIsMobile } from '@/hooks/use-mobile'
-import { Menu, PanelLeft } from 'lucide-react'
+import { Menu, PanelLeft, RefreshCw } from 'lucide-react'
 import { useBookingModal } from '@/hooks/use-booking-modal'
+import { useCallback, useMemo } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import { useKitchen } from '@/hooks/use-kitchen'
+import { toast } from 'sonner'
+import { kitchens } from '@/types/skapto-kitchens'
+import { usePathname } from 'next/navigation'
 
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const queryClient = useQueryClient()
+  const pathname = usePathname()
   const isMobile = useIsMobile()
   const { toggleSidebar } = useSidebar()
   const { toggleModal } = useBookingModal()
+  const { kitchen } = useKitchen()
+
+  const refetchBookings = useCallback(async () => {
+    await queryClient.refetchQueries({
+      queryKey: [ 'events', 'all', kitchen ]
+    })
+    toast.info(`Fetched the latest bookings for ${Object.entries(kitchens).find(([ _key, value ]) => value === kitchen)?.[0]} kitchen`)
+  }, [ queryClient, kitchen ])
+
+  const reloadButtonAllowedPaths = useMemo(() => [ '/dashboard', '/dashboard/bookings' ], [])
 
   return (
     <>
@@ -22,15 +40,34 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         {/* Static header/trigger */}
         <div className="flex justify-between items-center p-4 border-b shrink-0">
           {/* Button for opening sidebar/nav drawer */}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
+
+            {/* Open sidebar */}
+
             <Button variant={isMobile ? 'outline' : 'ghost'} onClick={toggleSidebar}>
               {isMobile ? <Menu /> : <PanelLeft />}
             </Button>
+
+            {/* Refetch bookings (desktop-only) */}
+
+            {!isMobile && reloadButtonAllowedPaths.includes(pathname) && (
+              <Button
+                variant={'ghost'}
+                onClick={refetchBookings}
+              >
+                <RefreshCw />
+              </Button>
+            )}
+
+            {/* Create Booking (mobile-only) */}
+
             {isMobile && <Button variant={'outline'} onClick={toggleModal}>
               Add Booking
             </Button>}
           </div>
-          {/* Add the user menu for mobile view */}
+
+          {/* Add the user menu (mobile-only) */}
+
           {isMobile && <UserMenu isMobile />}
         </div>
 
