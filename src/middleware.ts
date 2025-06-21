8569244e12bb16297a26bd2691ffce923f7579a8
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2025-06-21 16:15:22 +0300

    :hammer: Allow api calls through mw

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,13 +7,13 @@ import { composeMiddleware } from '@/middlewares'
 export function middleware(request: NextRequest) {
   const chain = composeMiddleware([
     auth,
-    defaultKitchen,
+    defaultKitchen
   ])
 
   return chain(request)
 }
 
+// todo add a more sophisticated matcher
 export const config = {
-  // Everything except next & assets
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)'
+  matcher: [ '/((?!api|_next/static|_next/image|.*\\.png$).*)' ]
 }
